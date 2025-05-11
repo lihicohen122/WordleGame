ef278cc8b18a24a6eca18f3654f1af8e68e81b24
OmriL <Omri.levy451@gmail.com>
2025-05-11 20:10:27 +0300

    initial commit of repository

---
+++ Ex02 Lihi 314958042 Omri 208008649/Ex02_UI/GameInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_UI
+{
+    internal class GameInterface
+    {
+        //private GameManager m_GameManager
+        private InputValidation m_InputValidation;
+    }
+
+    
+}
---
+++ Ex02 Lihi 314958042 Omri 208008649/Ex02_UI/InputValidation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_UI
+{
+    internal struct InputValidation
+    {
+        private string m_UserInput;
+    }
+}
